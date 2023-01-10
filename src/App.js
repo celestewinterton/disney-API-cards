e8d0a05072439bebf973d996760da4328a52c107
Celeste Winterton <celestewinterton@gmail.com>
2023-01-10 13:54:40 -0800

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Card from "./Components/Card";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from App</h1>
+      <h2>frontend/src/App.js</h2>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div>
+      <h1>Hello from Card</h1>
+      <h2>frontend/src//Components/Card.js</h2>
+    </div>
+  );
+}
+
+export default Card;
